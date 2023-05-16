6ee9778fd92ed35452a1269427d08c8e5e9ed4ed
Raí Adriano <raivieiraadriano92@gmail.com>
2023-05-16 20:13:40 -0300

    refactor: add support to path alias

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,18 @@ module.exports = function (api) {
   api.cache(true)
 
   return {
+    plugins: [
+      [
+        'module-resolver',
+        {
+          root: ['.'],
+          alias: {
+            assets: './assets',
+            src: './src'
+          }
+        }
+      ]
+    ],
     presets: ['babel-preset-expo']
   }
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
+import type { FunctionComponent } from 'react'
+
 import { StatusBar } from 'expo-status-bar'
 import { StyleSheet, Text, View } from 'react-native'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  )
-}
+export const App: FunctionComponent = () => (
+  <View style={styles.container}>
+    <Text>Open up App.tsx to start working on your app!</Text>
+    <StatusBar style="auto" />
+  </View>
+)
 
 const styles = StyleSheet.create({
   container: {
