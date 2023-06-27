bc6eff05103b854f8f27fcee0c62cb5439870e4a
Raí Adriano <raivieiraadriano92@gmail.com>
2023-06-27 11:39:34 -0300

    perf: set up enabledFreeze true

--- src/AppProviders.tsx
+++ src/AppProviders.tsx
@@ -4,6 +4,9 @@ import 'src/i18n'
 import type { FunctionComponent, PropsWithChildren } from 'react'
 
 import { NavigationContainer } from '@react-navigation/native'
+import { enableFreeze } from 'react-native-screens'
+
+enableFreeze(true)
 
 export const AppProviders: FunctionComponent<PropsWithChildren> = ({ children }) => (
   <NavigationContainer>{children}</NavigationContainer>
