a08a67a478631b34f256666b7abd64fdde2cf350
Raí Adriano <raivieiraadriano92@gmail.com>
2025-03-01 18:00:15 -0300

    setup nativewind with rn-reusables

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,5 +1,26 @@
 import { View } from "react-native";
 
+import { Button } from "~/components/ui/button";
+import { Text } from "~/components/ui/text";
+import { setAndroidNavigationBar } from "~/lib/android-navigation-bar";
+import { useColorScheme } from "~/lib/useColorScheme";
+
 export default function HomeScreen() {
-  return <View />;
+  const { isDarkColorScheme, setColorScheme } = useColorScheme();
+
+  function toggleColorScheme() {
+    const newTheme = isDarkColorScheme ? "light" : "dark";
+
+    setColorScheme(newTheme);
+
+    setAndroidNavigationBar(newTheme);
+  }
+
+  return (
+    <View className="flex-1 items-center justify-center">
+      <Button onPress={toggleColorScheme}>
+        <Text>Toggle Theme</Text>
+      </Button>
+    </View>
+  );
 }
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,36 +1,87 @@
-import { useEffect } from "react";
+import "~/global.css";
 
-import { useFonts } from "expo-font";
+import * as React from "react";
+
+import {
+  Theme,
+  ThemeProvider,
+  DefaultTheme,
+  DarkTheme
+} from "@react-navigation/native";
 import { Stack } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
 import { StatusBar } from "expo-status-bar";
-import "react-native-reanimated";
+import { Platform } from "react-native";
+
+import { setAndroidNavigationBar } from "~/lib/android-navigation-bar";
+import { NAV_THEME } from "~/lib/constants";
+import { useColorScheme } from "~/lib/useColorScheme";
+
+const LIGHT_THEME: Theme = {
+  ...DefaultTheme,
+  colors: NAV_THEME.light
+};
+
+const DARK_THEME: Theme = {
+  ...DarkTheme,
+  colors: NAV_THEME.dark
+};
+
+export {
+  // Catch any errors thrown by the Layout component.
+  ErrorBoundary
+} from "expo-router";
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
+SplashScreen.setOptions({
+  duration: 5000,
+  fade: true
+});
+
 export default function RootLayout() {
-  const [loaded] = useFonts({
-    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf")
-  });
+  const hasMounted = React.useRef(false);
 
-  useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
+  const { colorScheme, isDarkColorScheme } = useColorScheme();
+
+  const [isColorSchemeLoaded, setIsColorSchemeLoaded] = React.useState(false);
+
+  useIsomorphicLayoutEffect(() => {
+    if (hasMounted.current) {
+      return;
     }
-  }, [loaded]);
 
-  if (!loaded) {
+    if (Platform.OS === "web") {
+      // Adds the background color to the html element to prevent white background on overscroll.
+      document.documentElement.classList.add("bg-background");
+    }
+
+    setAndroidNavigationBar(colorScheme);
+
+    setIsColorSchemeLoaded(true);
+
+    hasMounted.current = true;
+
+    SplashScreen.hideAsync();
+  }, []);
+
+  if (!isColorSchemeLoaded) {
     return null;
   }
 
   return (
-    <>
+    <ThemeProvider value={isDarkColorScheme ? DARK_THEME : LIGHT_THEME}>
+      <StatusBar style={isDarkColorScheme ? "light" : "dark"} />
       <Stack>
         <Stack.Screen name="(tabs)" />
         <Stack.Screen name="+not-found" options={{ title: "Oops!" }} />
       </Stack>
-      <StatusBar style="auto" />
-    </>
+    </ThemeProvider>
   );
 }
+
+const useIsomorphicLayoutEffect =
+  Platform.OS === "web" && typeof window === "undefined"
+    ? React.useEffect
+    : React.useLayoutEffect;
