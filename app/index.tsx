6a431736a8bc0f2d5ca5789b5049ec306cb6b80c
Raí Adriano <raivieiraadriano92@gmail.com>
2025-03-03 15:56:10 -0300

    refactor WelcomeScreen layout for improved spacing and organization

--- app/index.tsx
+++ app/index.tsx
@@ -31,53 +31,47 @@ export default function WelcomeScreen() {
   };
 
   return (
-    <View className="p-safe flex-1">
-      <View className="flex-1 justify-center p-6">
-        {/* Logo Section */}
-        <View className="flex-1 items-center justify-center">
-          <Image
-            source={require("assets/images/icon.png")}
-            className="mb-6 h-24 w-24"
+    <View className="p-safe m-6 flex-1 gap-10">
+      {/* Logo Section */}
+      <View className="flex-1 items-center justify-center">
+        <Image
+          source={require("assets/images/icon.png")}
+          className="h-24 w-24"
+        />
+      </View>
+
+      {/* Title Section */}
+      <View className="items-center gap-3">
+        <H1 className="text-center">Start Your Fasting Journey Today!</H1>
+        <P className="text-center">
+          Welcome to Fastify, your all-in-one solution for intermittent fasting
+          success. Let's get started on journey to better health and well-being!
+        </P>
+      </View>
+
+      {/* Auth Buttons Section */}
+      <View className="gap-3">
+        {Platform.OS === "ios" && (
+          <SignInButton
+            provider="apple"
+            onPress={() => handleSignIn("apple")}
+            isLoading={isLoading.apple}
           />
-        </View>
-
-        {/* Title Section */}
-        <View className="items-center gap-3 pb-16">
-          <H1 className="text-center">Start Your Fasting Journey Today!</H1>
-          <P className="text-center">
-            Welcome to Fastify, your all-in-one solution for intermittent
-            fasting success. Let's get started on journey to better health and
-            well-being!
-          </P>
-        </View>
-
-        {/* Auth Buttons Section */}
-        <View className="gap-3">
-          {Platform.OS === "ios" && (
-            <SignInButton
-              provider="apple"
-              // onPress={() => handleSignIn("apple")}
-              onPress={() =>
-                toast.error("An error occurred. Please try again later.")
-              }
-              isLoading={isLoading.apple}
-            />
-          )}
-          {Platform.OS === "android" && (
-            <SignInButton
-              provider="google"
-              onPress={() => handleSignIn("google")}
-              isLoading={isLoading.google}
-            />
-          )}
-          {__DEV__ && (
-            <SignInButton
-              provider="anonymous"
-              onPress={() => handleSignIn("anonymous")}
-              isLoading={isLoading.anonymous}
-            />
-          )}
-        </View>
+        )}
+        {Platform.OS === "android" && (
+          <SignInButton
+            provider="google"
+            onPress={() => handleSignIn("google")}
+            isLoading={isLoading.google}
+          />
+        )}
+        {__DEV__ && (
+          <SignInButton
+            provider="anonymous"
+            onPress={() => handleSignIn("anonymous")}
+            isLoading={isLoading.anonymous}
+          />
+        )}
       </View>
     </View>
   );
